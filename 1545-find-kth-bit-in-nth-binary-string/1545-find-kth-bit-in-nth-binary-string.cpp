43a2072df05b59a0087285b76abdc4111bf20d2c
komal-chavan9090 <155181393+komal-chavan9090@users.noreply.github.com>
2024-02-09 18:01:53 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (72.75%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cpp
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cpp
@@ -1,36 +1,57 @@
+// class Solution {
+// public:
+
+//     string invert(string x)
+//     {
+//         for(int i=0;i<x.size();i++)
+//         {
+//             if(x[i]=='0')
+//             {
+//                 x[i]='1';
+//             }
+//             else
+//             {
+//                 x[i]='0';
+//             }
+            
+//         }
+//         reverse(x.begin(),x.end());
+//         return x;
+//     }
+//     string solve(int n)
+//     {
+//         if(n==1)
+//         {
+//             return "0";
+//         }
+
+//         return solve(n-1)+"1"+invert(solve(n-1));
+//     }
+    
+//     char findKthBit(int n, int k) {
+       
+//         string s= solve(n);
+//         return s[k-1];
+//     }
+// };
+
+
 class Solution {
 public:
-
-    string invert(string x)
-    {
-        for(int i=0;i<x.size();i++)
-        {
-            if(x[i]=='0')
-            {
-                x[i]='1';
-            }
-            else
-            {
-                x[i]='0';
-            }
-            
-        }
-        reverse(x.begin(),x.end());
-        return x;
-    }
-    string solve(int n)
-    {
-        if(n==1)
-        {
-            return "0";
-        }
-
-        return solve(n-1)+"1"+invert(solve(n-1));
-    }
-    
     char findKthBit(int n, int k) {
-       
-        string s= solve(n);
-        return s[k-1];
+        if(n==1){
+            return '0'; 
+        }
+        if(pow(2,n)/k==2){
+            return '1';
+        }
+        if(k<pow(2,n)/2){
+            return findKthBit(n-1,k);
+        }
+        k = pow(2,n)-k;
+        if(findKthBit(n-1,k)=='1'){
+            return '0';
+        }
+        return '1';
     }
 };
