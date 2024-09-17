97c5667461dc5daf03de4c279deb7a5e7169aeb1
komal-chavan9090 <155181393+komal-chavan9090@users.noreply.github.com>
2024-09-17 21:31:01 +0530

    Time: 2 ms (51.8%), Space: 9.1 MB (22.25%) - LeetHub

--- 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.cpp
+++ 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.cpp
@@ -3,22 +3,11 @@ public:
     vector<string> uncommonFromSentences(string s1, string s2) {
         string str=s1+" "+s2;
         stringstream ss(str);
-
         map<string,int> mpp;
         vector<string> ans;
-
         string word;
-        while(ss>>word)
-        {
-            mpp[word]++;
-        }
-        for(auto it:mpp)
-        {
-            if(it.second==1)
-            {
-                ans.push_back(it.first);
-            }
-        }
+        while(ss>>word) {mpp[word]++;}
+        for(auto it:mpp) { if(it.second==1) ans.push_back(it.first);  }
         return ans;
     }
 };
