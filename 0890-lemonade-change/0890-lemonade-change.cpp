7caa6f23f2007452d7a03702cceb90b179a37434
komal-chavan9090 <155181393+komal-chavan9090@users.noreply.github.com>
2024-12-10 10:38:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.cpp
+++ 0890-lemonade-change/0890-lemonade-change.cpp
@@ -1,25 +1,39 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-        int fivebillcount=0,tenbillcount=0;
-        for(int i=0;i<bills.size();i++){
-            if(bills[i]==5) fivebillcount++;
-            else if(bills[i]==10){
-                if(fivebillcount>=1){
-                    tenbillcount++;
-                    fivebillcount--;
-                }
-                else return false;
+
+        int five=0,ten=0,twe=0;
+        for(int i=0;i<bills.size();i++)
+        {
+            if(bills[i]==5)
+            {
+                five++;
             }
-            else{
-                if(tenbillcount>=1 && fivebillcount>=1){
-                    tenbillcount--;
-                    fivebillcount--;
+            else if(bills[i]==10)
+            {
+                if(five>0)
+                {
+                    five--;
                 }
-                else if(fivebillcount>=3){
-                    fivebillcount-=3;
+                else
+                {
+                    return false;
                 }
-                else return false;
+                ten++;
+            }
+            else if(bills[i]==20)
+            {
+                if(ten>0 && five>0)
+                {
+                    ten--;
+                    five--;
+                }
+                else
+                {
+                    return false;
+                }
+                
+                twe++;
             }
         }
         return true;
