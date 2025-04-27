c2283e134d4f89354eff79f18da8d9b28758a2fd
komal-chavan9090 <155181393+komal-chavan9090@users.noreply.github.com>
2025-04-27 20:56:39 +0530

    Time: 24 ms (13.94%), Space: 22.7 MB (51.72%) - LeetHub

---
+++ 2190-count-common-words-with-one-occurrence/2190-count-common-words-with-one-occurrence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countWords(vector<string>& w1, vector<string>& w2) {
+        map<string,int> map1,map2;
+        int count=0;
+
+        for(int i=0;i<w1.size();i++) map1[w1[i]]++;
+        for(int i=0;i<w2.size();i++) map2[w2[i]]++;
+
+        for(int i=0;i<w1.size();i++)
+        {
+            if(map1[w1[i]]==1 && map2[w1[i]]==1)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+};
