cf5d9bad1da65fe66991e6d34bca81d3bbcab8d7
komal-chavan9090 <155181393+komal-chavan9090@users.noreply.github.com>
2025-04-29 23:52:39 +0530

    Time: 47 ms (11.71%), Space: 27.5 MB (27.39%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
-    int helper(string &text1, string &text2, int m, int n, vector<vector<int>> &dp){
-        if(m==0 or n==0) return 0;
-        if(dp[m][n]!=-1) return dp[m][n];
-        if(text1[m-1]==text2[n-1]) dp[m][n]=1+helper(text1,text2,m-1,n-1,dp);
-        else dp[m][n]=max(helper(text1,text2,m,n-1,dp), helper(text1,text2,m-1,n,dp));
+    int helper(string &text1, string &text2, int m, int n, vector<vector<int>> &dp) {
+        if (m == 0 || n == 0) return 0;
+        if (dp[m][n] != -1) return dp[m][n];
 
-    return dp[m][n];
+        if (text1[m - 1] == text2[n - 1]) {
+            dp[m][n] = 1 + helper(text1, text2, m - 1, n - 1, dp);
+        } else {
+            dp[m][n] = max(helper(text1, text2, m, n - 1, dp),
+                           helper(text1, text2, m - 1, n, dp));
+        }
+        return dp[m][n];
     }
 
-    int longestCommonSubsequence(string text1, string text2){
-        int m=text1.size(), n=text2.size();
-        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
-
-    return helper(text1, text2, m, n, dp);
+    int longestCommonSubsequence(string text1, string text2) {
+        int m = text1.size(), n = text2.size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
+        return helper(text1, text2, m, n, dp);
     }
 };
