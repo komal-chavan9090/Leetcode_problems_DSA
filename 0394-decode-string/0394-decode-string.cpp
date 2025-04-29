5eb8f94279dc2040c4ba68a97e6c37946f35609a
komal-chavan9090 <155181393+komal-chavan9090@users.noreply.github.com>
2025-04-29 18:19:57 +0530

    Time: 0 ms (100%), Space: 8.6 MB (91.81%) - LeetHub

--- 0394-decode-string/0394-decode-string.cpp
+++ 0394-decode-string/0394-decode-string.cpp
@@ -1,42 +1,35 @@
 class Solution {
 public:
     string decodeString(string s) {
-        stack<string>st;
-        string ss="";
-        for(int i=0;i<s.length();i++)
-        {
-            string dig="";
-            if(isdigit(s.at(i)))
-            {
-                dig+=s.at(i);
-                i++;
-                while(i<s.length() && isdigit(s.at(i)))
-                {
-                    dig+=s.at(i);
-                    i++;
-                }
-                i--;
-                st.push(dig);
-                
-            }
-            else if (s.at(i) == '[') {
-                st.push("[");
-                st.push(ss); 
-                ss = ""; 
-            }
-            else if(s.at(i)>='a' && s.at(i)<='z')
-            ss=ss+s.at(i);
-            else if (s.at(i) == ']') {
-                string prev = st.top(); st.pop();
-                st.pop();
-                int nums = stoi(st.top()); st.pop();
+        stack<string> strStack;
+        stack<int> numStack;
+        string currStr = "";
+        int num = 0;
 
-                string temp = "";
-                for (int i = 0; i < nums; i++) {
-                    temp += ss;
+        for (char c : s) {
+            if (isdigit(c)) {
+                num = num * 10 + (c - '0');  // support for multi-digit numbers
+            }
+            else if (c == '[') {
+                numStack.push(num);
+                strStack.push(currStr);
+                num = 0;
+                currStr = "";
+            }
+            else if (c == ']') {
+                string temp = currStr;
+                currStr = strStack.top(); strStack.pop();
+                int repeatTimes = numStack.top(); numStack.pop();
+
+                while (repeatTimes--) {
+                    currStr += temp;
                 }
-                ss = prev + temp;            }
+            }
+            else {
+                currStr += c;
+            }
         }
-        return ss;
+
+        return currStr;
     }
 };
