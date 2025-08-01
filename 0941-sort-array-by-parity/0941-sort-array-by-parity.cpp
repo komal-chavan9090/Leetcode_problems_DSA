220df4f134c7419972b92559b3109f50188c4a6a
komal-chavan9090 <155181393+komal-chavan9090@users.noreply.github.com>
2025-08-01 21:56:10 +0530

    Time: 0 ms (100%), Space: 20.6 MB (67.29%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.cpp
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.cpp
@@ -1,31 +1,54 @@
-class Solution {
+
+        // vector<int> even;
+        // vector<int> odd;
+
+        // for(int i=0;i<nums.size();i++)
+        // {
+        //     if(nums[i]%2==0)
+        //     {
+        //         even.push_back(nums[i]);
+        //     }
+        //     else
+        //     {
+        //         odd.push_back(nums[i]);
+        //     }
+        // }
+
+        // for(int i=0;i<even.size();i++)
+        // {
+        //     nums[i]=even[i];
+        // }
+        // // int j=even.size()
+
+        // for(int i=0;i<odd.size();i++)
+        // {
+        //      nums[i+even.size()]=odd[i];
+        // }
+        // return nums;
+
+
+        class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
-        vector<int> even;
-        vector<int> odd;
+        int i = 0;
+        int j = nums.size() - 1;
 
-        for(int i=0;i<nums.size();i++)
-        {
-            if(nums[i]%2==0)
-            {
-                even.push_back(nums[i]);
+        while (i < j) {
+            // Move i forward while it's even
+            while (i < j && nums[i] % 2 == 0) {
+                i++;
             }
-            else
-            {
-                odd.push_back(nums[i]);
+            // Move j backward while it's odd
+            while (i < j && nums[j] % 2 != 0) {
+                j--;
             }
-        }
-
-        for(int i=0;i<even.size();i++)
-        {
-            nums[i]=even[i];
-        }
-        // int j=even.size()
-
-        for(int i=0;i<odd.size();i++)
-        {
-             nums[i+even.size()]=odd[i];
+            // Swap if both pointers stopped on wrong elements
+            if (i < j) {
+                swap(nums[i], nums[j]);
+            }
         }
         return nums;
     }
 };
+
+
